30bc9d1eea2a7965e6ce5e9abff03c0333ce9711
Abdulaziz Yusuf <mystore.uz@gmail.com>
2026-06-05 16:24:56 +0500

    fixed employee add form

--- src/app/components/employee/data.ts
+++ src/app/components/employee/data.ts
@@ -140,7 +140,7 @@ export class EmployeeComponent implements OnInit {
 
   addData() {
     const modalRef = this.modalService.open(EmployeeModal, { backdrop: 'static', windowClass: 'animated fadeInDown' });
-    modalRef.componentInstance.data = { no: '', date: '', company: '', type: 'outcome' };
+    modalRef.componentInstance.data = { phone_ext: 998, phone: '', name: '', access: 'active', password: '', role: 'employee', department: '' };
     modalRef.componentInstance.edited = true;
 
     modalRef.result.then((result) => {
